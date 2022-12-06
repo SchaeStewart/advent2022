abea7b0375ad6314522e46a0ed46a20a07adb4fa
Schaffer Stewart <schaffer.stewart@lifeomic.com>
2022-12-06 10:09:52 -0500

    day06 refactor

--- day06/index.ts
+++ day06/index.ts
@@ -1,3 +1,4 @@
+import { slidingWindows } from "https://deno.land/std@0.167.0/collections/sliding_windows.ts";
 import { readInput } from "../readInput.ts";
 
 const input = (await readInput("./input.txt"))[0];
@@ -8,14 +9,11 @@ const input = (await readInput("./input.txt"))[0];
 // const input = "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw" // first marker after character 11
 
 const findUniqueMarker = (input: string, size = 4): number => {
-  for (let i = 0; i < input.length; i++) {
-    if ([...new Set([...input.slice(i, i + size)])].length === size) {
-      return i + size;
-    }
-  }
-  return -1;
+  return slidingWindows([...input], size)
+    .map(window => [...new Set(window)])
+    .findIndex(window => window.length === size)
+    +size
 };
-
 
 console.log("Solution 1:", findUniqueMarker(input, 4));
 console.log("Solution 2:", findUniqueMarker(input, 14));
