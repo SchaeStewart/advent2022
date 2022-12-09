ff7c1d7ba69f6c3c0c1988163d827a7fabc25e58
Schaffer Stewart <schaffer.stewart@lifeomic.com>
2022-12-09 15:09:47 -0500

    day09 refactor

--- day09/index.ts
+++ day09/index.ts
@@ -15,31 +15,18 @@ const input = parseInput(await readInput("./input.txt"));
 
 const updateTail = (tail: Position, head: Position): Position => {
   let { x, y } = tail;
-  const updateX = () => {
-    if (tail.x > head.x) {
-      x -= 1;
-    } else {
-      x += 1;
-    }
-  };
-  const updateY = () => {
-    if (tail.y > head.y) {
-      y -= 1;
-    } else {
-      y += 1;
-    }
-  };
-  if (
-    (Math.abs(tail.x - head.x) > 1 && Math.abs(tail.y - head.y) >= 1) ||
-    (Math.abs(tail.x - head.x) >= 1 && Math.abs(tail.y - head.y) > 1)
-  ) {
+  const xDiff = Math.abs(tail.x - head.x);
+  const yDiff = Math.abs(tail.y - head.y);
+  const updateX = () => (x = tail.x > head.x ? x - 1 : x + 1);
+  const updateY = () => (y = tail.y > head.y ? y - 1 : y + 1);
+  if ((xDiff > 1 && yDiff >= 1) || (xDiff >= 1 && yDiff > 1)) {
     updateX();
     updateY();
     return { x, y };
   }
-  if (Math.abs(tail.x - head.x) > 1) {
+  if (xDiff > 1) {
     updateX();
-  } else if (Math.abs(tail.y - head.y) > 1) {
+  } else if (yDiff > 1) {
     updateY();
   }
   return { x, y };
@@ -65,36 +52,8 @@ const key = (p: Position) => `x:${p.x},y:${p.y}`;
 const updatePositions = (
   [d, amt]: Instruction,
   head: Position,
-  tail: Position,
-  visited: Map<string, number>
-) => {
-  for (let i = 0; i < amt; i++) {
-    head = updateHead(head, [d, 1]);
-    tail = updateTail(tail, head);
-    const c = visited.get(key(tail)) || 0;
-    visited.set(key(tail), c + 1);
-  }
-
-  return [head, tail];
-};
-
-const solvePart1 = (instructions: Instruction[]) => {
-  const visited = new Map<string, number>();
-  let head: Position = { x: 0, y: 0 };
-  let tail: Position = { x: 0, y: 0 };
-  visited.set(key(tail), 1);
-  for (const ins of instructions) {
-    [head, tail] = updatePositions(ins, head, tail, visited);
-  }
-
-  console.log("Part 1:", visited.size);
-};
-
-const updatePositionsMultiple = (
-  [d, amt]: Instruction,
-  head: Position,
   knots: Position[],
-  visited: Map<string, number>
+  visited: Set<string>
 ): [Position, Position[]] => {
   let tail = {...knots[0]}
   for (let i = 0; i < amt; i++) {
@@ -106,23 +65,22 @@ const updatePositionsMultiple = (
       knots[i] = {...tail}
       h = tail
     }
-    const c = visited.get(key(tail)) || 0;
-    visited.set(key(tail), c + 1);
+    const c = visited.add(key(tail))
   }
 
   return [head, knots];
 };
 
-const solvePart2 = (instructions: Instruction[]) => {
-  const visited = new Map<string, number>();
+const countVisitedByTail = (instructions: Instruction[], knotCount: number) => {
   let head: Position = { x: 0, y: 0 };
-  let knots: Position[] = new Array(9).fill({ x: 0, y: 0 });
-  visited.set(key(knots.at(-1)!), 1);
+  let knots: Position[] = new Array(knotCount).fill({ x: 0, y: 0 });
+  const visited = new Set<string>([key(head)]);
   for (const ins of instructions) {
-    [head, knots] = updatePositionsMultiple(ins, head, knots, visited);
+    [head, knots] = updatePositions(ins, head, knots, visited);
   }
-  console.log("Part 2:", visited.size);
+
+  return visited.size;
 };
 
-solvePart1(input);
-solvePart2(input);
+console.log("Part 1", countVisitedByTail(input, 1));
+console.log("Part 2", countVisitedByTail(input, 9));
