62a6bab9f6c9fc08ded2e441e9ade2b8e1a70e92
Schaffer Stewart <schaffer.stewart@lifeomic.com>
2022-12-12 22:55:39 -0500

    day12 part2

--- day12/index.ts
+++ day12/index.ts
@@ -26,7 +26,7 @@ const buildGraph = (input: string[][]): Map<string, Graph> => {
     for (let x = 0; x < input[y].length; x++) {
       const v = input[y][x];
       const g: Graph = {
-        value: v === "S" ? 0 : v === "E" ? 26 : v.charCodeAt(0) - 96,
+        value: v === "S" ? 1 : v === "E" ? 26 : v.charCodeAt(0) - 96,
         x: x,
         y: y,
         loc: key({ x, y }),
@@ -44,9 +44,6 @@ const buildGraph = (input: string[][]): Map<string, Graph> => {
   }
 
   for (const g of m.values()) {
-    if (g.loc === end) {
-      continue;
-    }
     // check all cardinal dir values.
     const north = m.get(key({ ...g, y: g.y - 1 }));
     if (north && north.value - g.value <= 1) {
@@ -70,7 +67,6 @@ const buildGraph = (input: string[][]): Map<string, Graph> => {
 
 const traverse = (map: Map<string, Graph>): number => {
   const visited = new Set<string>();
-  const order: string[] = [];
   const queue = new BinaryHeap<Graph>((a, b) => {
     if (a.distance >= b.distance) {
       return 1;
@@ -92,16 +88,84 @@ const traverse = (map: Map<string, Graph>): number => {
       }
     }
     visited.add(g.loc);
-    order.push(g.loc);
   }
-  // console.log(JSON.stringify(order, null, 2));
-  console.log({ start, end });
-  console.log(map.get(key({ x: 43, y: 20 })));
   return map.get(end)?.distance || -1;
 };
 
-const g = buildGraph(parseInput(raw));
-console.log(traverse(g));
+const buildBackwardGraph = (input: string[][]): Map<string, Graph> => {
+  const m = new Map<string, Graph>();
+  for (let y = 0; y < input.length; y++) {
+    for (let x = 0; x < input[y].length; x++) {
+      const v = input[y][x];
+      const g: Graph = {
+        value: v === "S" ? 1 : v === "E" ? 26 : v.charCodeAt(0) - 96,
+        x: x,
+        y: y,
+        loc: key({ x, y }),
+        vertices: [],
+        distance: v === "E" ? 0 : Number.MAX_SAFE_INTEGER,
+      };
+      if (v === "E") {
+        end = g.loc;
+      }
+      if (v === "S") {
+        start = g.loc;
+      }
+      m.set(g.loc, g);
+    }
+  }
 
-// console.log("Part 1", solvePart1(parseInput(raw)));
-// console.log("Part 2", solvePart2(parseInput(raw)));
+  for (const g of m.values()) {
+    // check all cardinal dir values.
+    const north = m.get(key({ ...g, y: g.y - 1 }));
+    if (north && g.value - north.value <= 1) {
+      g.vertices.push(north);
+    }
+    const south = m.get(key({ ...g, y: g.y + 1 }));
+    if (south && g.value - south.value <= 1) {
+      g.vertices.push(south);
+    }
+    const east = m.get(key({ ...g, x: g.x + 1 }));
+    if (east && g.value - east.value <= 1) {
+      g.vertices.push(east);
+    }
+    const west = m.get(key({ ...g, x: g.x - 1 }));
+    if (west && g.value - west.value <= 1) {
+      g.vertices.push(west);
+    }
+  }
+  return m;
+};
+
+const trail = (map: Map<string, Graph>): number => {
+  const visited = new Set<string>();
+  const queue = new BinaryHeap<Graph>((a, b) => {
+    if (a.distance >= b.distance) {
+      return 1;
+    } else {
+      return -1;
+    }
+  });
+  queue.push(map.get(end)!);
+  let last = Number.MAX_SAFE_INTEGER;
+  while (queue.length > 0) {
+    const g = queue.pop()!;
+    const step = g.distance + 1;
+    for (const v of g.vertices) {
+      const n = map.get(key(v));
+      if (n && !visited.has(key(n)) && n.distance > step) {
+        n.distance = step;
+        if (n.value !== 1) {
+          queue.push(n);
+        } else {
+          last = Math.min(last, step);
+        }
+      }
+    }
+    visited.add(g.loc);
+  }
+  return last;
+};
+
+console.log("Part 1", traverse(buildGraph(parseInput(raw))));
+console.log("Part 2", trail(buildBackwardGraph(parseInput(raw))));
